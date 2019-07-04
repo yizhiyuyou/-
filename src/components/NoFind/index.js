470dff35dff94d96c7f0b51ecdc12983bc7aabc5
yizhiyuyou <949267840@qq.com>
2019-07-04 17:46:55 +0800

    增加 403,404 模块

--- src/components/NoFind/index.js
+++ src/components/NoFind/index.js
@@ -1,5 +1,24 @@
 import React from 'react'
 
-export default () => {
-  return <div>没有找到你想要的东西</div>
+import { Result, Button } from 'antd'
+
+import styles from './index.module.less'
+
+export default ({ history }) => {
+  const RESULT_PROPS = {
+    status: '404',
+    title: '404',
+    subTitle: '抱歉,你访问的页面不存在.',
+    extra: (
+      <Button type="primary" onClick={() => history.replace('/home/')}>
+        回首页
+      </Button>
+    ),
+  }
+
+  return (
+    <div className={styles.container}>
+      <Result {...RESULT_PROPS} className={styles.result} />
+    </div>
+  )
 }
--- src/components/NoPermission/index.js
+++ src/components/NoPermission/index.js
@@ -1,5 +1,24 @@
 import React from 'react'
 
-export default () => {
-  return <div>你没有这个权限</div>
+import { Result, Button } from 'antd'
+
+import styles from './index.module.less'
+
+export default ({ history }) => {
+  const RESULT_PROPS = {
+    status: '403',
+    title: '403',
+    subTitle: '抱歉,你无权访问此页面.',
+    extra: (
+      <Button type="primary" onClick={() => history.replace('/home/')}>
+        回首页
+      </Button>
+    ),
+  }
+
+  return (
+    <div className={styles.container}>
+      <Result {...RESULT_PROPS} className={styles.result} />
+    </div>
+  )
 }
